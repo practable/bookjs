a958816d4916e9d2fab09e84c9336ba6319b16f3
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-01-15 22:57:46 +0000

    Initial commit project template

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # bookjs
-Javascript booking client for timdrysdale/relay
+
+Empty project.
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To run in hot module reloading mode:
+
+```sh
+npm start
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+## Running
+
+```sh
+node dist/bundle.js
+```
+
+## Testing
+
+To run unit tests:
+
+```sh
+npm test
+```
+
+## Credits
+
+Made with [createapp.dev](https://createapp.dev/)
+
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <h1>
+      {{name}}
+    </h1>
+    <button type="button" class="btn btn-primary">
+      This is a bootstrap button
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import 'bootstrap';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+
+  export default Vue.extend({
+    data: function() {
+      return {
+        name: 'Hello World!',
+      }
+    },
+  });
+</script>
+
+<style>
+h1 {
+  color: white;
+  background-color: black;
+}
+</style>
