aa096dfa67ff9dd1f2e8801bb8589017eaa11058
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-01-16 19:31:01 +0000

    Add axios in component (works) try/login

--- try/login/src/components/enterToken.vue
+++ try/login/src/components/enterToken.vue
@@ -2,11 +2,20 @@
   <input :value="token" @input="updateToken" />
   <button @click="login">login</button>
   <button @click="deleteToken">clear</button>
+  <button @click="getSomething">getSomething</button>
+  <div>What we got: {{ stuff }}</div>
 </template>
 
 <script>
 import { mapActions, mapState } from "vuex";
+import axios from "axios";
+
 export default {
+  data() {
+    return {
+      stuff: null,
+    };
+  },
   computed: {
     ...mapState({
       token: (state) => state.token,
@@ -15,6 +24,23 @@ export default {
   methods: {
     updateToken(e) {
       this.$store.commit("setToken", e.target.value);
+    },
+    getSomething() {
+      console.log("getSomething() called");
+
+      axios
+        .get("https://api.npms.io/v2/search?q=vue")
+        .then((response) => (this.stuff = response.data.total));
+
+      /*
+		const { response, error, loading, uploadProgress, exec } = useAxios()
+
+	  await exec({
+        url: 'https://rickandmortyapi.com/api/character',
+        method: 'get'
+      })
+	console.log(	await !error.value ? await response.value : null)
+*/
     },
     ...mapActions(["login", "deleteToken"]),
   },
--- try/login/src/main.js
+++ try/login/src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from "vue";
 import store from "./store";
-
 import App from "./app.vue";
 
 const app = createApp(App); //displayBearer); //, enterToken });
 
-app.use(store);
+app.use(store)
 
 app.mount("#app");
