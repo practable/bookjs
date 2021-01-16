2fb0cf4671a6442e60528dfe8a118a093044e53f
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-01-16 15:05:26 +0000

    Refactor as counter try/answer2

--- try/answer2/src/main.js
+++ try/answer2/src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import store from "./store";
+import CounterControls from "./counterControls.vue";
 
-createApp(App).mount('#app')
+const app = createApp(CounterControls);
+
+app.use(store);
+
+app.mount("#app");
