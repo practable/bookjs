2437433506bb39f047b91836d29af9932bf4e4b5
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-01-27 15:32:48 +0000

    Change session request to 1/5/30/60/90min

--- src/src/components/describePool.vue
+++ src/src/components/describePool.vue
@@ -22,7 +22,8 @@
 				  <li @click="request(90)"  role="presentation"><a role="menuitem" href="#" id="btn1">90 min</a></li>
 				  <li @click="request(60)"  role="presentation"><a role="menuitem" href="#" id="btn2">60 min</a></li>
 				  <li @click="request(30)"  role="presentation"><a role="menuitem" href="#" id="btn3">30 min</a></li>
-				  <li @click="request(0.1)"   role="presentation"><a role="menuitem" href="#" id="btn4">6 sec</a></li>
+				  <li @click="request(5)"  role="presentation"><a role="menuitem" href="#" id="btn3">5 min</a></li>
+				  <li @click="request(1)"   role="presentation"><a role="menuitem" href="#" id="btn4">1 min</a></li>
 				</ul>
 			  </div>
 		    </div>
