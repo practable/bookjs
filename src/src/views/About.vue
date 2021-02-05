5e35353bf23b23e3f9600ca54a973bb75cff4f64
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-02-05 18:34:26 +0000

    Fix layour of About view

--- src/src/views/About.vue
+++ src/src/views/About.vue
@@ -1,14 +1,50 @@
 <template>
-  <div class="container-fluid align-center">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col"></div>
+    <div class="col text-justify">
 
 <h1>About</h1>
 
-<p> This is a proof-of-concept booking system for developers and course staff to explore the activities in their current state of development. Further refinements are expected, including the addition of features and minor/cosmetic changes to existing features. Expect changes in the way that you authenticate to the system, to make it more convenient than present.</p>
-
-<h2> Authentication </h2>
 <p>
-  Please enter the token you have been given into the text box on the <a href="/login">catalogue</a> page.
-</p>
+This is the booking interface for the <a href="https://practable.io">practable.io</a> non-traditional practical work system. </p>
+
+	  <h3> For Users </h3>
+
+	  	  <p>The booking system, and the <a href="https://practable.io">practable.io</a> system itself, are under active development. You will notice functional and cosmetic changes over time. These should be largely self-explanatory.</p>
+	
+
+	<p> It's entirely possible you will find the odd issue, despite our best efforts to eliminate them. A simple page reload, or returning to the original link you received, should fix most of the issues that might crop up. We'd be delighted to hear from you if you have any other issues, or feedback. Just drop us a line at <a href="mailto:support@practable.io">support@practable.io</a> with as much detail as you can manage.</p>	  
+
+	<h3> For Educators </h3>
+	
+	<p> Practable is the core reference implemention of an emerging consensus regarding the role of remote laboratories in on-campus education. Some of the background can be found <a href="https://doi.org/10.1080/23752696.2020.1816845">here</a>.</p>
+
+	<h3> Adopters and Developers</h3>
+	<p>	If you are a potential adopter or developer of federated remote laboratory technologies, you might be pleasantly surprised to find that all the code and hardware is open-source, for the complete feature set. We will continue to develop the code base in the open.  We value the opportunity to create educational software which reflects our values. We hope you do too.</p>
+
+	<p> If you want to take a quick look at the software, you can find the core <a href="https://github.com/timdrysdale/relay">here</a>. Developer and adopter documentation is on our roadmap, as are support tools and services to set you up and/or keep you going / scale up. Alternatively, we can do some or all of the work for you - get in touch to discuss the options without obligation. If you have any questions, want to discuss getting some help, or simply want to let us know you might be interested in participating in the practable ecosystem in some way, let us know by emailing <a href="mailto:admin@practable.io">Tim</a>.</p>
+
+	</div>
+	<div class="col"></div>
+  </div>
 </div>
+<b-container class="bv-example-row">
+  <b-row>
+    <b-col> </b-col>
+    <b-col>
+<b-container>
+	
+</b-container>
+	  </b-col>
+    <b-col> </b-col>
+  </b-row>
+</b-container>
+
+ 
+
+
+<!---div class="container-fluid" style="max-width: 540px;">
+	<div class="row align-center"--->
 
 </template> 
