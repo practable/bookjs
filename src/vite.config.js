ad89a071a4a1390ad1ecf76a61438843e47fa2f0
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-01-31 10:48:56 +0000

    config (fix): put base path in env var

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 const path = require("path");
-export default defineConfig({
-  base: "/book/",
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // import.meta.env.VITE_BASE available here with: process.env.VITE_BASE
+
+  return defineConfig({
+    base: process.env.VITE_BASE,
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+  });
+};
