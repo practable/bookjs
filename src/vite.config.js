1f88b06b94e2d3269ad0e630992316ad5fb876b8
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-01-31 10:28:22 +0000

    scripts (fix): now pushes correctly to static repo

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 const path = require("path");
 export default defineConfig({
+  base: "/book/",
   plugins: [vue()],
   resolve: {
     alias: {
