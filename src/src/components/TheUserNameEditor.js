a7f16c98333001fe326a9b3d9aeedace8db8373f
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-02-03 18:11:54 +0000

    TheUserNameEditor (test): mock store

--- src/src/components/TheUserNameEditor.js
+++ src/src/components/TheUserNameEditor.js
@@ -12,6 +12,7 @@ export default {
   },
   methods: {
     getNewUserName() {
+      var $this = this;
       axios
         .post(import.meta.env.VITE_APP_BOOK_SERVER + "/api/v1/users/unique")
         .then(
@@ -19,23 +20,26 @@ export default {
             if (response.data) {
               console.log("userName.js: /users/unique", response.data);
               if (response.data.user_name) {
-                return response.data.user_name;
+                if (this.isXID(response.data.user_name)) {
+                  console.log(
+                    "userName.js: valid new user name from /users/unique",
+                    response.data.user_name
+                  );
+                  $this.userName = response.data.user_name;
+                  $this.storeUserName(response.data.user_name);
+                }
               } else {
                 console.log(
-                  "userName.js: /users/unique",
-                  "no user_name in response"
+                  "userName.js: /users/unique no user_name in response"
                 );
-                return "";
               }
             } else {
               console.log("userName.js: /users/unique", "no data response");
-              return "";
             }
           },
           (error) => {
             if (error.response) {
               console.log("userName.js: /users/unique", error.response);
-              return "";
             }
           }
         );
@@ -60,33 +64,24 @@ export default {
         userName
       );
 
-      userName = this.getNewUserName();
-
-      if (this.isXID(userName)) {
-        console.log(
-          "userName.js: valid userName obtained from server",
-          userName
-        );
-        this.updateUserName(userName);
-        return;
-      }
-
-      console.log(
-        "userName.js: problem with userName obtained from server (must be XID)",
-        userName
-      );
-      return "";
+      this.getNewUserName(); //will store the result as well
     },
     isXID(str) {
       // https://github.com/rs/xid
       // To validate a base32 xid, expect a 20 chars long, all lowercase sequence of a to v letters and 0 to 9 numbers ([0-9a-v]{20}).
+      if (!str) {
+        //if undefined
+        return false;
+      }
+      if (str.length != 20) {
+        return false;
+      }
       const regex = new RegExp("[0-9a-v]{20}");
       return regex.test(str);
     },
-    updateUserName(userName) {
-      console.log("userName.js: new userName is", userName);
-      this.userName = userName;
-      localStorage.set("userName", userName);
+    storeUserName(userName) {
+      console.log("userName.js: storing new userName is", userName);
+      localStorage.setItem("userName", userName);
       this.$store.commit("setUserName", userName);
     },
   },
--- src/src/components/__tests__/TheUserNameEditor.spec.js
+++ src/src/components/__tests__/TheUserNameEditor.spec.js
@@ -1,21 +1,60 @@
 import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
 import { shallowMount, flushPromises } from "@vue/test-utils";
 import TheUserNameEditor from "../TheUserNameEditor.vue";
-//import axios from "axios";
+import axios from "axios";
+import { createStore } from "vuex";
 
 // https://testdriven.io/blog/vue-unit-testing/
 // Mock the axios library
-//vi.mock("axios", () => {
-//  return {
-//    default: {
-//      get: vi.fn(), //mock get
-//      post: vi.fn(), //mock post
-//    },
-//  };
-//});
+vi.mock("axios", () => {
+  return {
+    default: {
+      get: vi.fn(), //mock get
+      post: vi.fn(), //mock post
+    },
+  };
+});
 
-describe("TheUserNameEditor.vue test successfully get name in local storage", () => {
-  it("renders userName when component is created", async () => {
+describe("TheUserNameEditor.vue test isXID", () => {
+  beforeEach(() => {
+    localStorage.setItem("userName", "00000000000000000000");
+  });
+  it("check 9m4e2mr0ui3e8a215n4g is valid", () => {
+    const wrapper = shallowMount(TheUserNameEditor);
+    expect(wrapper.vm.isXID("9m4e2mr0ui3e8a215n4g")).toBe(true);
+  });
+  it("check cfejfguot5un1fu2era0 is valid", () => {
+    const wrapper = shallowMount(TheUserNameEditor);
+    expect(wrapper.vm.isXID("cfejfguot5un1fu2era0")).toBe(true);
+  });
+  it("check string including an x,y, or z is invalid", () => {
+    const wrapper = shallowMount(TheUserNameEditor);
+    expect(wrapper.vm.isXID("cfejfguot5un1fu2erax")).toBe(false);
+  });
+  it("check string including punctuation is invalid", () => {
+    const wrapper = shallowMount(TheUserNameEditor);
+    expect(wrapper.vm.isXID("cfejfguot5un1fu2era:")).toBe(false);
+  });
+  it("check short string is invalid", () => {
+    const wrapper = shallowMount(TheUserNameEditor);
+    expect(wrapper.vm.isXID("cfejfguot5un1fu2era")).toBe(false);
+  });
+  it("check long string is invalid", () => {
+    const wrapper = shallowMount(TheUserNameEditor);
+    expect(wrapper.vm.isXID("cfejfguot5un1fu2era01")).toBe(false);
+  });
+  it("check string with capitalised letter is invalid", () => {
+    const wrapper = shallowMount(TheUserNameEditor);
+    expect(wrapper.vm.isXID("cFejfguot5un1fu2era01")).toBe(false);
+  });
+});
+
+describe("TheUserNameEditor.vue test successfully get name", () => {
+  beforeEach(() => {
+    axios.get.mockReset();
+  });
+
+  it("renders userName from localStorage when component is created", async () => {
     // render the component
     localStorage.setItem("userName", "9m4e2mr0ui3e8a215n4g");
 
@@ -25,7 +64,41 @@ describe("TheUserNameEditor.vue test successfully get name in local storage", ()
 
     // check that the userName is extracted from localStorage
     expect(wrapper.vm.userName).toMatch("9m4e2mr0ui3e8a215n4g");
+    expect(wrapper.text()).toContain("9m4e2mr0ui3e8a215n4g");
+  });
 
-    // TODO check that the user name is rendered
+  it("renders userName from server if none in local storage when component is created", async () => {
+    //https://stackoverflow.com/questions/69330156/how-to-add-fake-store-in-shallowmount-using-vue3-typescript-in-jest-unit-tes
+
+    const store = createStore({
+      state() {
+        return {
+          userName: "testuser",
+        };
+      },
+      mutations: {
+        setUserName(state, userName) {
+          state.userName = userName;
+        },
+      },
+    });
+    // render the component
+
+    localStorage.setItem("userName", "");
+    axios.post.mockResolvedValue({
+      data: { user_name: "cfejfguot5un1fu2era0" },
+    });
+
+    const wrapper = shallowMount(TheUserNameEditor, {
+      global: {
+        plugins: [store],
+      },
+    });
+
+    await flushPromises(); //wait til DOM updates
+
+    expect(axios.post).toHaveBeenCalledWith("undefined/api/v1/users/unique");
+    expect(wrapper.vm.userName).toMatch("cfejfguot5un1fu2era0");
+    expect(wrapper.text()).toContain("cfejfguot5un1fu2era0");
   });
 });
