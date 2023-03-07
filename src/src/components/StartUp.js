6c76b592375b94068398501961aeb91bb5389e9f
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-03-07 11:19:38 +0000

    StartUp (fix): add short query params
    We want to shorten query params for issue 21, as well as stop always adding
    g-everyone by default (part of the fix for issues 20).
    
    [Issue: 20,21]

--- src/src/components/StartUp.js
+++ src/src/components/StartUp.js
@@ -8,7 +8,7 @@ export default defineComponent({
   computed: {},
   mounted() {
     //repeated query param keys are mixing the results so don't do that
-    let gn = ["g-everyone"]; //default group
+    let gn = []; //no default group - see below for optional addition of g-everyone
     let sn = [];
 
     // example of query params - use JSON format for arrays, including quotes
@@ -26,6 +26,27 @@ export default defineComponent({
     }
     if (this.route.query.session) {
       sn.push(this.route.query.session);
+    }
+
+    // short version: gg for groups, ss for sessions
+    if (this.route.query.gg) {
+      gn = JSON.parse(this.route.query.gg);
+    }
+    if (this.route.query.ss) {
+      sn = JSON.parse(this.route.query.ss);
+    }
+
+    // short version: g for group, s for session
+    if (this.route.query.g) {
+      gn.push(this.route.query.g);
+    }
+    if (this.route.query.session) {
+      sn.push(this.route.query.s);
+    }
+
+    // optionally add default g-everyone group
+    if (gn.length <= 0 && sn.length <= 0) {
+      gn = ["g-everyone"]; //if no query params, add the g-everyone group
     }
 
     let sg = {
